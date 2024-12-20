66a0e7a81dae0f905e752cab00dcb5ee6d26ceb3
Chaitanya Wankhede <chaitanyawankhede705@gmail.com>
2024-12-20 19:56:25 +0530

    moved navbar to layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import NavBar from "@/components/NavBar";
+// import NavBar from "@/components/NavBar";
 
 export default function Home() {
   return (
     <div className="ma">
-      <NavBar/>
+      Hello
     </div>
   );
 }
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -7,7 +7,7 @@ export default function NavBar() {
         <nav className="flex items-center justify-between p-4 bg-white shadow-md dark:bg-background border-b-2">
             <div className="flex items-center space-x-4">
                 <Image src="/pencil.ico" alt="Logo" width={32} height={32} />
-                <span className="text-2xl sm:text-sm font-bold text-gray-900 dark:text-white">MyApp</span>
+                <span className="text-2xl sm:text-sm font-bold text-gray-900 dark:text-white">Notes</span>
             </div>
             <div className="flex items-center space-x-4">
                 <Button>Login</Button>
