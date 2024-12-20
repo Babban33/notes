ba609bc12865ecd8d7edb44aa7e7bf93fe759ac6
Chaitanya Wankhede <chaitanyawankhede705@gmail.com>
2024-12-20 14:25:58 +0530

    added dark theme toggle

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import { ModeToggle } from "./modeToggle";
+
 export default function Home() {
   return (
     <div>
       Hello World
+      <div>
+        <ModeToggle/>
+      </div>
     </div>
   );
 }
